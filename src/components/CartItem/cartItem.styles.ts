50223f86a380917c9dee26fe2dd8f7a5b5d4bfe6
the wiki <olusegundavids1@gmail.com>
2023-09-09 16:34:35 +0100

    On branch master

--- src/components/CartItem/cartItem.styles.ts
+++ src/components/CartItem/cartItem.styles.ts
@@ -5,16 +5,19 @@ export const Wrapper = styled.div`
     justfy-content: space-between;
     flex-direction: column;
     width: 100%;
-    border: 1px solid lightblue;
+    border: 1px solid #BD83B8;
     border-radius: 20px;
     height: 100%
 
     button{
-        border-radius: 0 0 20px 20px;
+        /* border: red 20px; */
+        border-radius: 20px 20px 20px 20px;
     }
 
     button:hover{
         background: #BD83B8;
+        border-radius: 20px 20px 20px 20px;
+
     }
 
     p{
@@ -26,7 +29,9 @@ export const Wrapper = styled.div`
     }
 
     img{
-        max-height: 250px;
+        /* max-height: 5rem; */
+        width: 100%;
+        height: 10rem;
         object-fit: cover;
         border-radius: 20px 20px 0 0;
     }
